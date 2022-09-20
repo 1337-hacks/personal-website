218c963df0971957f4041e24e8b637698b59bfec
1337-hacks <elijahnucum.exe@gmail.com>
2022-09-20 18:06:09 +1200

    c002 Front page developments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const presetFiles = [{fileName: "Project:NOTES"}, {fileName: "Personal Website (This Website!)"}, {fileName: "About Elijah"}]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-body">
+
+        {presetFiles.map((file, index) => {
+          const posOffset = index * 12;
+          let offsetLeft = `calc(40% + ${posOffset}px)`;
+          return(
+            <div 
+              key={index}
+              className="file"
+              style={{left: offsetLeft}}
+            >
+              <p style={{fontSize: "15px"}}>{file.fileName}</p>
+            </div>
+          )
+        })}
     </div>
   );
 }
---
+++ src/LoadingScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoadingScreen() {
+    return(0);
+};
+
+export default LoadingScreen;
